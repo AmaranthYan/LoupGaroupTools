2cc043cdcc99d7f39cf28f48f436510830a13840
Amaranth YAN <amaranth.yan@gmail.com>
2016-06-12 01:14:34 +0200

    为聊天信息添加发送玩家标签；使用PlayerPrefs本地存储玩家名称

--- Assets/Scripts/NetworkViews/SetPlayerNameView.cs
+++ Assets/Scripts/NetworkViews/SetPlayerNameView.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SetPlayerNameView : MonoBehaviour
 {
+    private const string PLAYER_PERSISTENT_NAME_KEY = "player_persistent_name";
+
+    [SerializeField]
+    private InputField m_PlayerNameField = null;
+
+    void Start()
+    {
+        if (m_PlayerNameField)
+        {
+            m_PlayerNameField.text = PlayerPrefs.GetString(PLAYER_PERSISTENT_NAME_KEY);
+        }        
+    }
+
     public void SetPlayerName(string playerName)
     {
         PhotonNetwork.playerName = playerName;
+        PlayerPrefs.SetString(PLAYER_PERSISTENT_NAME_KEY, PhotonNetwork.playerName);
     }
 }
