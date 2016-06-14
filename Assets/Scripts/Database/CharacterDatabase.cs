5c40855e368471a4e2f5a69ee9c371a1687a3a4d
Amaranth YAN <amaranth.yan@gmail.com>
2016-06-15 00:06:53 +0200

    IdentityModel更名为CharacterModel；添加CharacterDatabase角色数据库，添加角色定义

---
+++ Assets/Scripts/Database/CharacterDatabase.cs
@@ -0,0 +1,14 @@
+﻿namespace LoupsGarous
+{
+    using UnityEngine;
+    using System;
+    using System.Collections;
+
+    [CreateAssetMenu(fileName = "LoupsGarousCharacterDatabase", menuName = "LoupsGarous/CharacterDatabase", order = 1)]
+    public class CharacterDatabase : ScriptableObject
+    {
+        [SerializeField]
+        private CharacterModel[] m_CharacterModels;
+        public CharacterModel[] CharacterModels { get { return m_CharacterModels; } }
+    }
+}
---
+++ Assets/Scripts/Models/CharacterModel.cs
@@ -0,0 +1,33 @@
+﻿namespace LoupsGarous
+{
+    using UnityEngine;
+    using System;
+    using System.Collections.Generic;
+
+    [Serializable]
+    public class CharacterModel
+    {
+        [SerializeField]
+        private int m_Id;
+        [SerializeField]
+        private int m_Camp;
+        [SerializeField]
+        private string m_DisplayName;
+        [SerializeField]
+        private string m_AmountRegex;
+        [SerializeField]
+        private int m_CollateralCharacters;
+        [SerializeField]
+        private Texture2D m_Icon;
+        [SerializeField]
+        private Texture2D m_Image;
+
+        public int Id { get { return m_Id; } }
+        public int Camp { get { return m_Camp; } }
+        public string DisplayName { get { return m_DisplayName; } }
+        public string AmountRegex { get { return m_AmountRegex; } }
+        public int CollateralCharacters { get { return m_CollateralCharacters; } }
+        public Texture2D Icon { get { return m_Icon; } }
+        public Texture2D Image { get { return m_Image; } }
+    }
+}
