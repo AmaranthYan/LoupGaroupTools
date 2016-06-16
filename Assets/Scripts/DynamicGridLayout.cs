be004f2c2b841e732e0ec5d7497924428adfe7aa
Amaranth YAN <amaranth.yan@gmail.com>
2016-06-17 01:49:02 +0200

    添加DynamicGrid脚本，实现Cell尺寸动态自适应容器大小；添加CharacterSettingViewPrefab

---
+++ Assets/Scripts/DynamicGridLayout.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[RequireComponent(typeof(GridLayoutGroup))]
+public class DynamicGridLayout : MonoBehaviour
+{
+    private RectTransform m_GridRectTransform = null;
+    [SerializeField]
+    private GridLayoutGroup m_GridLayoutGroup = null;
+
+    void Awake()
+    {
+        m_GridRectTransform = this.GetComponent<RectTransform>();
+        m_GridLayoutGroup = this.GetComponent<GridLayoutGroup>();
+    }
+
+    void Start()
+    {
+        ResizeCell();
+    }
+    
+    void OnRectTransformDimensionsChange()
+    {
+        ResizeCell();
+    }
+
+    private void ResizeCell()
+    {
+        if (!m_GridRectTransform || !m_GridLayoutGroup) { return; }
+
+        Vector2 cellSize = m_GridLayoutGroup.cellSize;
+	    switch (m_GridLayoutGroup.constraint)
+        {
+            case GridLayoutGroup.Constraint.FixedColumnCount:
+                cellSize.x = m_GridRectTransform.rect.width / m_GridLayoutGroup.constraintCount;
+                m_GridLayoutGroup.cellSize = cellSize;
+                break;
+            case GridLayoutGroup.Constraint.FixedRowCount:
+                cellSize.y = m_GridRectTransform.rect.height / m_GridLayoutGroup.constraintCount;
+                m_GridLayoutGroup.cellSize = cellSize;
+                break;
+            case GridLayoutGroup.Constraint.Flexible:
+                break;
+        }
+	}
+}
