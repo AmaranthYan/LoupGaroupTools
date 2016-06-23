25475567b7e0a53238f79b6f933aa9d6c6214939
Amaranth YAN <amaranth.yan@gmail.com>
2016-06-23 02:44:42 +0200

    调整项目文件结构；添加SettingCacheService；添加CharacterSetting数据模型

---
+++ Assets/Scripts/DataPersistence/CharacterSetting.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+public class CharacterSetting
+{
+    private int m_Id;
+    private string m_Name;
+    private bool m_IsEnabled;
+    private int m_Amount;
+
+    [XmlAttribute("id")]
+    public int Id { get { return m_Id; } set { m_Id = value; } }
+    [XmlAttribute("name")]
+    public string Name { get { return m_Name; } set { m_Name = value; } }
+    public bool IsEnabled { get { return m_IsEnabled; } set { m_IsEnabled = value; } }
+    public int Amount { get { return m_Amount; } set { m_Amount = value; } }
+}
