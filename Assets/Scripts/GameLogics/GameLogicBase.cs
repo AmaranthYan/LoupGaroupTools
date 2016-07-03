64aac1f614287ed55d5e77352b5f217ba2db1806
Amaranth YAN <amaranth.yan@gmail.com>
2016-07-03 03:14:51 +0200

    实装IEnumerable的Shuffle扩展方法；修改GameLogicBase；修改GenericGameLogic，添加随机生成玩家编号

--- Assets/Scripts/GameLogics/GameLogicBase.cs
+++ Assets/Scripts/GameLogics/GameLogicBase.cs
@@ -7,16 +7,30 @@
 
     public abstract class GameLogicBase : PunBehaviour
     {
+        protected delegate void GameLogicCallback();
+
         [SerializeField]
         protected CharacterDatabase m_CharacterDatabase = null;
 
         protected PhotonPlayer[] m_Players = null;
         protected Dictionary<int, int> m_CharacterSet = null;
 
-        public void InitGameLogic(PhotonPlayer[] players, Dictionary<int, int> characterSet)
+        protected Dictionary<int, PhotonPlayer> m_PlayerIdentities = new Dictionary<int, PhotonPlayer>();
+        protected List<CharacterModel> m_UnusedCharacters = new List<CharacterModel>();
+
+        protected GameLogicCallback m_InitGameLogic_Callback = null;
+
+        public virtual void InitGameLogic(PhotonPlayer[] players, Dictionary<int, int> characterSet)
         {
             m_Players = players;
             m_CharacterSet = characterSet;
+
+            m_InitGameLogic_Callback();
         }
+
+        public abstract void GeneratePlayerIdentities();
+        public abstract void DistributePlayerIdentities();
+        public abstract void BroadcastPlayerIdentity(PhotonPlayer player);
+        protected abstract void ReceivePlayerIdentity(PhotonPlayer player, int characterId);
     }
 }
--- Assets/Scripts/GameLogics/GenericGameLogic.cs
+++ Assets/Scripts/GameLogics/GenericGameLogic.cs
@@ -11,17 +11,51 @@
         {
             if (PhotonNetwork.isMasterClient)
             {
-                GeneratePlayerIdentities();
+                GameLogicCallback handler = null;
+                handler = () => {
+                    GeneratePlayerIdentities();
+                    m_InitGameLogic_Callback -= handler;
+                };
+                m_InitGameLogic_Callback += handler;
             }
         }
 
-        private void GeneratePlayerIdentities()
+        #region PhotonCallbacks
+        //若上帝玩家(发生MasterPlayer切换)离开则强制结束游戏进程
+        public override void OnMasterClientSwitched(PhotonPlayer newMasterClient)
         {
-            //列表0号预留给MasterClient
+            base.OnMasterClientSwitched(newMasterClient);
+
+            if (PhotonNetwork.player.Equals(newMasterClient))
+            {
+                FindObjectOfType<GameSessionServiceView>().EndGameSession();
+            }
+        }
+        #endregion
+
+        public override void GeneratePlayerIdentities()
+        {
+            if (!PhotonNetwork.isMasterClient) { return; }
+
+            //随机生成玩家编号，0号预留给上帝玩家(MasterPlayer)
+            List<int> playerNumbers = new List<int>();
+            for (int i = 1; i < m_Players.Length; i++)
+            {
+                playerNumbers.Add(i);
+            }
+            playerNumbers.Shuffle();
+
+            //分配玩家编号
+            for (int i = 0; i < m_Players.Length;i++)
+            {
+                m_PlayerIdentities.Add(m_Players[i].isMasterClient ? 0 : playerNumbers[i], m_Players[i]);
+                Debug.Log((m_Players[i].isMasterClient ? 0 : playerNumbers[i]) + " : " + m_Players[i]);
+            }
+
             DistributePlayerIdentities();
         }
 
-        public void DistributePlayerIdentities()
+        public override void DistributePlayerIdentities()
         {
             if (PhotonNetwork.isMasterClient)
             {
@@ -29,7 +63,7 @@
             }
         }
 
-        public void BroadcastPlayerIdentity(PhotonPlayer player)
+        public override void BroadcastPlayerIdentity(PhotonPlayer player)
         {
             if (PhotonNetwork.isMasterClient)
             {
@@ -37,7 +71,7 @@
         }
 
         [PunRPC]
-        private void ReceivePlayerIdentity(PhotonPlayer player, int characterId)
+        protected override void ReceivePlayerIdentity(PhotonPlayer player, int characterId)
         {
             if (player.Equals(PhotonNetwork.player))
             {
