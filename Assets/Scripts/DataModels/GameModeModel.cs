80289b063875080138f0895998fed4d91666d4f9
Amaranth YAN <amaranth.yan@gmail.com>
2016-07-03 03:19:52 +0200

    添加规则描述Description至GameModeModel

--- Assets/Scripts/DataModels/GameModeModel.cs
+++ Assets/Scripts/DataModels/GameModeModel.cs
@@ -14,11 +14,14 @@
         [SerializeField]
         private int[] m_DisabledCharacters;
         [SerializeField]
+        private string m_Description;
+        [SerializeField]
         private GameObject m_GameSessionPrefab;
 
         public int Id { get { return m_Id; } }
         public string DisplayName { get { return m_DisplayName; } }
         public int[] DisabledCharacters { get { return m_DisabledCharacters; } }
+        public string Description { get { return m_Description; } }
         public GameObject GameSessionPrefab { get { return m_GameSessionPrefab; } }
     }
 }
