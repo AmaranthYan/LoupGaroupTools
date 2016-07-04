68749cc56ab8bdd51a8e781026e2a2ae712357fd
Amaranth YAN <amaranth.yan@gmail.com>
2016-07-04 22:38:46 +0200

    添加GameVersionView，读取并显示游戏版本

---
+++ Assets/Scripts/UIViews/GameVersionView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameVersionView : MonoBehaviour
+{
+    [SerializeField]
+    private PhotonIdentifier m_PhotonIdentifierSettings = null;
+    [SerializeField]
+    private string m_GameVersionFormat = string.Empty;
+
+    public UnityTypedEvent.StringEvent onGameVersionInfoLoad = new UnityTypedEvent.StringEvent();
+
+    void Start()
+    {
+        if (m_PhotonIdentifierSettings)
+        {
+            onGameVersionInfoLoad.Invoke(string.Format(m_GameVersionFormat, m_PhotonIdentifierSettings.GameVersion));
+        }
+    }
+}
