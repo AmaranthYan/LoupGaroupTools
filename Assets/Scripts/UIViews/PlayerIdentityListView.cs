14a586a2e606b968c76a4b674de4e96133468aa8
Amaranth YAN <amaranth.yan@gmail.com>
2016-07-24 17:00:46 +0200

    添加PlayerIdentityView/PlayerIdentityListView，用于PlayerIdentity列表的UI显示；修改GameSessionGeneric的Prefab，添加UI组件并关联UI事件

---
+++ Assets/Scripts/UIViews/PlayerIdentityListView.cs
@@ -0,0 +1,23 @@
+﻿namespace LoupsGarous
+{
+    using UnityEngine.Events;
+    using System;
+    using System.Collections.Generic;
+
+    public class PlayerIdentityListView : ScrollListView<PlayerIdentity>
+    {
+        [Serializable]
+        public class SingleItemEvent : UnityEvent<PlayerIdentity> { }
+        [Serializable]
+        public class MultiItemEvent : UnityEvent<List<PlayerIdentity>> { }
+
+        public new SingleItemEvent onSelectedItemChange = new SingleItemEvent();
+        public new MultiItemEvent onMultiSelectedItemsChange = new MultiItemEvent();
+
+        protected override void Awake()
+        {
+            base.onSelectedItemChange = onSelectedItemChange;
+            base.onMultiSelectedItemsChange = onMultiSelectedItemsChange;
+        }
+    }
+}
