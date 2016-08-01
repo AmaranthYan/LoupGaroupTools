19d2901de564fd4bd7e43db014f7e4becd139d0b
Amaranth YAN <amaranth.yan@gmail.com>
2016-08-01 03:00:40 +0200

    添加CharacterDropdownView，显示所有游戏中的角色的Dropdown列表；修改PlayerIdentity，添加CharactersDropdown并关联参数引用

---
+++ Assets/Scripts/UIViews/CharactersDropdownView.cs
@@ -0,0 +1,45 @@
+﻿namespace LoupsGarous
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    [RequireComponent(typeof(Dropdown))]
+    public class CharactersDropdownView : MonoBehaviour
+    {
+        [SerializeField]
+        private Dropdown m_CharactersDropdown = null;
+        [SerializeField]
+        private CharacterDatabase m_CharacterDatabase = null;
+        [SerializeField]
+        private string m_UnknownCharacterText = string.Empty;
+
+        private GameLogicBase m_GameLogic = null;
+
+        void Start()
+        {
+            m_GameLogic = FindObjectOfType<GameLogicBase>();
+            m_CharactersDropdown = this.GetComponent<Dropdown>();
+
+            PopulateCharactersDropdown();
+        }
+
+        private void PopulateCharactersDropdown()
+        {
+            int[] characterIds = m_GameLogic.CharacterIds;
+
+            List<Dropdown.OptionData> optionList = new List<Dropdown.OptionData>();
+            optionList.Add(new Dropdown.OptionData(m_UnknownCharacterText));
+            foreach (int id in characterIds)
+            {
+                Dropdown.OptionData option = new Dropdown.OptionData(m_CharacterDatabase.CharacterModels[id].DisplayName, m_CharacterDatabase.CharacterModels[id].Image);
+                optionList.Add(option);
+            }
+
+            m_CharactersDropdown.options = optionList;
+        }
+    }
+}
